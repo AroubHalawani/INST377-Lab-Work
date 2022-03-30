f80650fc80b956c228a10ecf030d12ad318d88d8
ahalawa1 <47068569+ahalawa1@users.noreply.github.com>
2022-03-30 12:58:09 -0400

    Lab 6

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -1,15 +1,46 @@
-async function mainEvent() { // the async keyword means we can make API requests
-  const form = document.querySelector('.main_form');
-  form.addEventListener('submit', async (submitEvent) => { // async has to be declared all the way to get an await
-    submitEvent.preventDefault(); // This prevents your page from refreshing!
-    console.log('form submission'); // this is substituting for a "breakpoint"
-    const results = await fetch('/api/foodServicesPG'); // This accesses some data from our API
-    const arrayFromJson = await results.json(); // This changes it into data we can use - an object
-    console.table(arrayFromJson.data); // this is called "dot notation"
-    // arrayFromJson.data - we're accessing a key called 'data' on the returned object
-    // it contains all 1,000 records we need
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+  
+}
+function restoArrayMake(dataArray) {
+  console.log(dataArray.length);
+  console.log('fired dataHandler');
+  const range = [...Array(15).keys()];
+  const listItems = range.map((item) => {
+    const restNum = getRandomIntInclusive(0, dataArray.length - 1);
+    return dataArray[restNum];
+  });
+  console.log(listItems);
+  const targetList = document.querySelector('.resto-list');
+  targetList.innerHTML = '';
+  listItems.forEach((item) => {
+    const injectThisItem = `<li>${item.name}</li>`;
+    targetList.innerHTML += injectThisItem;
   });
 }
 
-// this actually runs first! It's calling the function above
+async function mainEvent() {
+  const form = document.querySelector('.main_form');
+  const submit = document.querySelector('.submitButton');
+  const results = await fetch('/api/foodServicesPG'); 
+  const arrayFromJson = await results.json();
+  
+  
+  console.log(form)
+  
+  submit.style.display = 'none';
+  if (arrayFromJson.data.length > 0) {
+    console.log(arrayFromJson);
+    submit.style.display = 'block';
+    form.addEventListener('submit', async (submitEvent) => {
+      submitEvent.preventDefault();
+      console.log('form submission');      
+      currArray = restoArrayMake(arrayFromJson.data);
+      console.log(currArray);
+    });
+  }
+}
+
 document.addEventListener('DOMContentLoaded', async () => mainEvent()); // the async keyword means we can make API requests
